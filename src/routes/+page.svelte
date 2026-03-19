ff2b8f32c9c8ed40089460aa69f21c5cd3bf290b
blckfrost <beataanima88@gmail.com>
2026-03-19 21:47:59 +0000

    project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<svelte lang="ts"></svelte>
+<main>
+	<div>
+		<h1>Draw together, in real time</h1>
+		<p>Create a room and share the link. Anyone with the link can draw with you instantly.</p>
+
+		<div>
+			<input type="text" placeholder="room name (or leave blank for random)" />
+		</div>
+	</div>
+</main>
